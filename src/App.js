4ef0753fc09baa5a2edf7bf63bcff92a7eb96442
depechecode <scunchen@umich.edu>
2020-08-04 19:42:44 -0400

    finish story

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import List from './List.js';
 import Add from './Add.js';
+import Home from './Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/list" component={List}></Route>
         <Route path="/add" component={Add}></Route>
       </Switch>
+      <Home></Home>
     </Router>
   );
 }
