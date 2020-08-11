9714650bac8d2b2252559cc0810953eb1f325823
depechecode <scunchen@umich.edu>
2020-08-10 20:40:05 -0400

    finish story

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import List from './List';
 import AddItem from './AddItem';
 import Home from './Home';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Welcome}></Route>
         <Route path="/list" component={List}></Route>
         <Route path="/add" component={AddItem}></Route>
       </Switch>
+      {/* <Welcome /> */}
       <Home />
     </Router>
   );
