cfead1bec5144a1c620ea58f143bbd3370515231
depechecode <scunchen@umich.edu>
2020-08-11 21:24:12 -0400

    add route for homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import List from './List';
 import AddItem from './AddItem';
-import Home from './Home';
 import Welcome from './Welcome';
 
 function App() {
@@ -14,8 +13,6 @@ function App() {
         <Route path="/list" component={List}></Route>
         <Route path="/add" component={AddItem}></Route>
       </Switch>
-      {/* <Welcome /> */}
-      <Home />
     </Router>
   );
 }
