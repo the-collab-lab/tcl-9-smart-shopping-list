8e24ed74e286e648c4e1bff411873974494954ec
Fabrice Innocent <fab33150@gmail.com>
2020-08-23 20:12:42 -0400

    Fixed conflicts

--- src/AddItem.js
+++ src/AddItem.js
@@ -4,6 +4,7 @@ import NavLinks from './NavLinks';
 import useTokenHook from './useTokenHook';
 
 const AddItem = () => {
+  const items = useContext(ItemsContext);
   const { token } = useTokenHook();
   const [inputValue, setInputValue] = useState();
   const [frequency, setFrequency] = useState(7);
@@ -11,17 +12,25 @@ const AddItem = () => {
 
   const addItem = event => {
     event.preventDefault();
+    const db = firebase.firestore();
+    const cleanInput = inputValue
+      .toLowerCase()
+      .trim()
+      .replace(/[.,\/#!$+%\^&\*;:{}=\-_`~()]/g, '');
 
-    return firebase
-      .firestore()
-      .collection('items')
-      .add({
-        token,
-        name: inputValue,
-        frequency: frequency,
-        lastPurchased: null,
-      })
-      .then(setSuccess(true));
+    const itemNames = items.map(data => data.name);
+    if (!itemNames.includes(cleanInput)) {
+      db.collection('items')
+        .add({
+          token,
+          name: cleanInput,
+          frequency: frequency,
+          lastPurchased: null,
+        })
+        .then(setSuccess(true));
+    } else {
+      alert(cleanInput + ' already exists on your list');
+    }
   };
 
   const handleOnChange = e => {
