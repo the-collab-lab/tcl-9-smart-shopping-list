ed242d8ad370fd5ebfc2da3b428612da20e28c04
depechecode <scunchen@umich.edu>
2020-08-24 18:57:34 -0400

    update lastPurchased prop

--- src/List.js
+++ src/List.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FirestoreCollection } from 'react-firestore';
+import firebase from 'firebase';
+
 import NavLinks from './NavLinks';
 import useTokenHook from './useTokenHook';
+import { ItemsContext } from './ItemsContext';
 
 const List = () => {
+  const items = useContext(ItemsContext);
+  const db = firebase.firestore();
   const { token } = useTokenHook();
+  const handleChange = e => {
+    // const checkedItem = items.filter(item => item.name === e.target.name)[0];
+    // console.log(checkedItem);
+    // checkedItem.lastPurchased = Date.now();
+    // console.log(checkedItem);
+    const newData = { lastPurchased: Date.now() };
+    var itemRef = db.collection('items').doc(e.target.id);
+    itemRef.update(newData);
+  };
 
   return (
     <FirestoreCollection
@@ -18,7 +32,15 @@ const List = () => {
             <h1>Items</h1>
             <ul>
               {data.map(items => (
-                <li key={items.id}>{items.name}</li>
+                <li key={items.id}>
+                  <input
+                    type="checkbox"
+                    onChange={handleChange}
+                    id={items.id}
+                    name={items.name}
+                  ></input>
+                  {items.name}
+                </li>
               ))}
             </ul>
             <NavLinks />
