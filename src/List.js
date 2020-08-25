4d3f988362be1df5d13bd57fef48327c7cd1d0a6
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-08-25 15:53:48 -0700

    switching branches won't let me unless I commit

--- src/List.js
+++ src/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { FirestoreCollection } from 'react-firestore';
 import NavLinks from './NavLinks';
 import useTokenHook from './useTokenHook';
@@ -6,6 +6,16 @@ import { ItemsContext } from './ItemsContext';
 
 const List = () => {
   const { token } = useTokenHook();
+  const items = useContext(ItemsContext);
+  const [emptyList, setEmptyList] = useState(false);
+
+  console.log('items', items);
+  // if items === empty token associated with items
+  // if (items.token === ) {
+  //   setEmptyList(true);
+  // }
+  // maybe have a useState true and false like in AddItem when empty set to true,
+  // false will show the list
 
   return (
     <FirestoreCollection
@@ -17,11 +27,15 @@ const List = () => {
         ) : (
           <div>
             <h1>Items</h1>
-            <ul>
-              {data.map(items => (
-                <li key={items.id}>{items.name}</li>
-              ))}
-            </ul>
+            {emptyList ? (
+              <span>Your list is empty! Please add an item</span>
+            ) : (
+              <ul>
+                {data.map(items => (
+                  <li key={items.id}>{items.name}</li>
+                ))}
+              </ul>
+            )}
             <NavLinks />
           </div>
         );
